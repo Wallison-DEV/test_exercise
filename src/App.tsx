d755587d09bb943c1ae791a7cd144b16cdb44278
Wallison <wallisonzwka@gmail.com>
2024-04-08 16:22:11 -0300

    feat/test-two-comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://images.tcdn.com.br/img/img_prod/747391/miniatura_batmovel_do_filme_batman_1989_michael_keaton_1_24_jada_toys_2133_1_20200414154226.jpg">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,30 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
 import PostComment from '.';
+import App from '../../App';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    test("Teste para a inserção de dois comentários", () => {
+        const {debug}=render(<App/>)
+        fireEvent.change(screen.getByTestId("comments-textarea"), { 
+            target:{
+                value: 'First comment'
+            }
+        })
+        fireEvent.click(screen.getByTestId('button-comment'))
+        expect(screen.getByText('First comment')).toBeInTheDocument()
+        fireEvent.change(screen.getByTestId("comments-textarea"), { 
+            target:{
+                value: 'Second comment'
+            }
+        })
+        fireEvent.click(screen.getByTestId('button-comment'))
+        expect(screen.getByText('Second comment')).toBeInTheDocument()
+        expect(screen.queryAllByTestId('post-comment')).toHaveLength(2)
+        debug()
+    })
 });
